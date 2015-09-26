8f04b820c469689f48d5db4b8b28a136b4808ace
Tomer Saban <sab30226@gmail.com>
2015-09-26 15:09:46 +0300

    Added reading of jsons with unicode suppport.

--- makefile
+++ makefile
@@ -5,6 +5,7 @@ install: install_dependencies
 install_dependencies:
 	sudo ./install_dependencies.sh
 
-test: res/input_html.html
-	python src/Main.py res/encrypted_key
+test: 
+	python src/Main.py res/sublet_in_telaviv_for_short_periods
+ 
 
--- src/Main.py
+++ src/Main.py
@@ -1,15 +1,57 @@
+import os
 import sys
+import codecs
+import json
+import pprint
 
 DEBUG = True
 
-class DataReciever:
-    def __init__(self):
-        
+#
+class UnicodePrettyPrinter(pprint.PrettyPrinter):
+    def format(self, object, context, maxlevels, level):
+        if isinstance(object, unicode):
+            return (object.encode('utf8'), True, False)
+        return pprint.PrettyPrinter.format(self, object, context, maxlevels, level)
+#
+
+class DataReceiver:
+    def __init__(self, group_feed_directory):
+        self.__message_queue = []
+
+        if not os.path.exists(group_feed_directory):
+            raise Exception("group feed directory doesn't exist: {0}".format(group_feed_directory))
+
+        if DEBUG:
+            for file in os.listdir(group_feed_directory)[:1]:
+                group_feed_file = os.path.join(group_feed_directory, file)
+
+                with codecs.open(group_feed_file, "r", encoding="utf-8") as gf_handle:
+                    data = json.load(gf_handle)
+                    UnicodePrettyPrinter().pprint(data['data'][0])
+
+        else:
+            for file in os.listdir(group_feed_directory):
+                group_feed_file = os.path.join(group_feed_directory, file)
+
+                with codecs.open(group_feed_file, "r", encoding="utf-8") as gf_handle:
+                    data = json.load(gf_handle)
+                    pprint(data)
+
+    def get_data(self):
+        return 0
+
+
+
+
 
 class Sublets:
+    def __init__(self, group_feed_directory):
+        self.__dr = DataReceiver(group_feed_directory)
+
     def main(self):
-        read_input_files()
+        pass
 
 if __name__ == "__main__":
-    sublets = Sublets()
+    group_feed_directory = sys.argv[1]
+    sublets = Sublets(group_feed_directory)
     sublets.main()
