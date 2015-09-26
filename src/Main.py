4f6815904e3de333fabda4c0e3246f1f5e3cdf0a
Tomer Saban <sab30226@gmail.com>
2015-09-26 19:00:20 +0300

    Added an example of Analyzed post which extracts the numbers from the message.

--- src/Main.py
+++ src/Main.py
@@ -3,10 +3,12 @@ import sys
 import codecs
 
 from DataReceiver import DataReceiver
+from Entities import AnalyzedPost
 
 class Sublets:
     def __init__(self, group_feed_directory):
         self.__dr = DataReceiver(group_feed_directory)
+        self.__anaylzed_posts = []
 
         os.makedirs("output")
 
@@ -14,6 +16,10 @@ class Sublets:
         for post in self.__dr.get_data():
             with codecs.open(os.path.join("output", post.id), "w", encoding='utf-8') as out:
                 out.write(post.message)
+
+            analyzed = AnalyzedPost(post)
+
+            print analyzed
 
 if __name__ == "__main__":
     group_feed_directory = sys.argv[1]
