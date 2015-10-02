ef996289ebccbf5c6d44b1dcad4822062cd19e81
Tomer Saban <sab30226@gmail.com>
2015-10-02 20:30:49 +0300

    Added the Areas.py file that is needed inorder to analyze the posts for the area.

---
+++ src/Areas.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+class Areas:
+    OLD_NORTH = "OLD_NORTH"
+    BASEL = "BASEL"
+    SHAPIRA = "SHAPIRA"
+    FLORENTIN = "FLORENTIN"
+    TEL_AVIV_CENTER = "TEL_AVIV_CENTER"
+    DIZINGOFF_SQUARE = "DIZINGOFF_SQUARE"
+    NACHALAT_BINYAMIN = "NACHALAT_BINYAMIN"
+    KIKAR_RABIN = "KIKAR_RABIN"
+    DIZINGOFF_CENTER = "DIZINGOFF_CENTER"
+    RAMAT_AVIV = "RAMAT_AVIV"
+    TAU = "TAU"
+    YAFO = "YAFO"
+    GAN_MEIR = "GAN_MEIR"
+
+    __AREAS = {OLD_NORTH: (u'צפון ישן', u'צפון הישן'),
+               BASEL: (u'בזל'),
+               SHAPIRA: (u'שפירא'),
+               FLORENTIN: (u'פלורנטין'),
+               TEL_AVIV_CENTER: (u'מרכז תל אביב'),
+               DIZINGOFF_SQUARE: (u'כיכר דיזינגוף' ,u' ככר דיזינגוף'),
+               NACHALAT_BINYAMIN: (u'נחלת בנימין'),
+               KIKAR_RABIN: (u'כיכר רבין' ,u' ככר רבין'),
+               DIZINGOFF_CENTER: (u'דיזינגוף סנטר' ,u' הסנטר'),
+               RAMAT_AVIV: (u'רמת אביב'),
+               TAU: (u'אוניברסיטת תל אביב' ,u' אוניברסיטת ת"א'),
+               YAFO: (u'יפו'),
+               GAN_MEIR: (u'גן מאיר')}
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_areas():
+        return Areas.__AREAS.keys()
+
+    @staticmethod
+    def get_area_words(area):
+        if area in Areas.__AREAS.keys():
+            return Areas.__AREAS[area]
+
+        return None
--- src/PostAnalyzer.py
+++ src/PostAnalyzer.py
@@ -1,5 +1,7 @@
 import re
 import Entities
+import Areas
+import types
 
 class PostAnalyzer:
     __post_analyzer = None
@@ -12,15 +14,29 @@ class PostAnalyzer:
         return PostAnalyzer.__post_analyzer
 
     def __init__(self):
-        self.__filters = [PostAnalyzer.get_numbers]
+        self.__extractor = [PostAnalyzer.get_area]
 
     def analyze_post(self, post):
         analysis = {}
-        for filter in self.__filters:
-            analysis[filter] = filter(post.message)
+        for extractor in self.__extractor:
+            analysis[extractor] = extractor(post.message)
 
         return Entities.AnalyzedPost(post, analysis)
 
     @staticmethod
     def get_numbers(message):
         return re.findall("\\d+", message)
+
+    @staticmethod
+    def get_area(message):
+        for area in Areas.Areas.get_areas():
+            words = Areas.Areas.get_area_words(area)
+
+            if not type(words) is type(()) and not type(words) is type([]):
+                words = [words]
+
+            for word in words:
+                if word in message:
+                    return area
+
+        return u'' #No area
