d53ffc6871b385d1dc1f762b95bdda6696da6d35
Tomer Saban <sab30226@gmail.com>
2017-09-16 16:58:14 +0300

    calc_results.sh:
    -Added shebang

--- calc_results.sh
+++ calc_results.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 echo "Prices found on $(for file in $(ls output/analyzed/); do cat output/analyzed/$file; echo ""; done | grep -v None | wc -l) messages"
 
 echo "Prices not found on $(for file in $(ls output/analyzed); do cat output/analyzed/$file; echo ""; done | grep None | wc -l) messages."
